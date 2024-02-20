cb7b837b71cc4fc5d81624bdca0e5a144ab3eda9
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2024-02-21 00:26:20 +0400

    fixed config

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	output: 'standalone',
+	images: {
+		domains: [process.env.API_URL.slice(0, -4)],
+	},
+	env: {
+		API_URL: process.env.API_URL,
+		API_TOKEN: process.env.API_TOKEN,
+		CAPTCHA_KEY: process.env.CAPTCHA_KEY,
+	},
+}
+
+export default nextConfig
