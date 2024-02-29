8947bd2e3b7e44e5f83bb328f854c243dfd67fdb
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2024-02-29 22:35:10 +0400

    connected new font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,9 @@
 import Head from 'next/head'
-import { useEffect } from 'react'
 import { Provider } from 'react-redux'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/app/globals.scss'
 import IndexWrapper from '@/components/Wrappers/IndexWrapper'
 import store from '../redux/store'
-import { Roboto } from 'next/font/google'
-
-const roboto = Roboto({
-	weight: ['400', '700'],
-	style: ['normal', 'italic'],
-	subsets: ['latin'],
-	display: 'swap',
-})
 // import favicon from '@/assets/img/favicon.webp'
 
 function MyApp({ Component, pageProps }) {
